a37224cf62c7d3283fd771c2f7f1be12c3291bb2
James <jameshaller27@gmail.com>
2021-05-08 11:51:52 -0700

    Log bot token

--- bot.py
+++ bot.py
@@ -19,4 +19,5 @@ if bot_token is None:
     print(f"'{token_env_name}' not found in environment. Failed to start bot.", file=sys.stderr)
     print(f"Go to https://discord.com/developers/applications/840469974983245844/bot + authenticate to retrieve token")
     sys.exit(1)
+print(f"'Token: {bot_token}'")
 bot.run(bot_token)
