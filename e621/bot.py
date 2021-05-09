7f512694018f7a9ff2594a1082007700d949adc7
James Haller <jameshaller27@gmail.com>
2021-05-08 23:52:29 -0700

    Refactor get_posts to instance method

--- e621/bot.py
+++ e621/bot.py
@@ -35,29 +35,38 @@ class CallerError(RuntimeError):
         return f"Failed call with code: {self.status_code}.\n" + self.text
 
 
-def get_posts(tags: list[str], limit: int = 5) -> list[Post]:
-    host = os.environ.get('E621_HOST') or 'https://e621.net'
-
-    tag_list = '+'.join(tags)
-    uri = f"{host}/posts.json?limit={limit}&tags={tag_list}"
-    headers = {
-        "User-Agent": "BlueBotNow/1.0 by Wizinja27"
-    }
-
-    response = requests.get(uri, headers=headers)
-
-    if response.status_code != 200:
-        raise CallerError(response)
-
-    data = response.json()
-    posts_obj = Posts.parse_obj(data)
-
-    return posts_obj.posts
-
-
 class E621(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: commands.Bot, username: str):
         self._bot = bot
+
+        self._bot_name = "BlueBotNow"
+        self._version = "1.0"
+        self._username = username
+
+    @property
+    def _user_agent_header(self) -> str:
+        # The default user agent for many programming languages and libraries have been blocked for abuse
+        # See API wiki for more information: https://e621.net/help/api
+        return f"{self._bot_name}/{self._version} by {self._username}"
+
+    def get_posts(self, tags: list[str], limit: int = 5) -> list[Post]:
+        host = os.environ.get('E621_HOST') or 'https://e621.net'
+
+        tag_list = '+'.join(tags)
+        uri = f"{host}/posts.json?limit={limit}&tags={tag_list}"
+        headers = {
+            "User-Agent": self._user_agent_header
+        }
+
+        response = requests.get(uri, headers=headers)
+
+        if response.status_code != 200:
+            raise CallerError(response)
+
+        data = response.json()
+        posts_obj = Posts.parse_obj(data)
+
+        return posts_obj.posts
 
     @commands.group()
     async def e621(self, ctx: commands.Context):
@@ -69,8 +78,8 @@ class E621(commands.Cog):
         tag_query = '+'.join(tags)
 
         try:
-            posts = get_posts(tags, limit)
-            embed = Embed(title="Search e621.net", url=f'https://e621.net/posts?tags={tag_query}')
+            posts = self.get_posts(tags, limit)
+            embed = Embed(title="Search on e621.net", url=f'https://e621.net/posts?tags={tag_query}')
 
             if len(posts) == 0:
                 embed.description = "No results for: " + ", ".join(tags)
@@ -93,4 +102,5 @@ class E621(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(E621(bot))
+    username = os.environ.get("E621_UNAME") or "(username not found)"
+    bot.add_cog(E621(bot, username))
