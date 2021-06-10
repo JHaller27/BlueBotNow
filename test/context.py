e8bdfe8dccbde6b581fda5f563147ce4aae23934
James <jameshaller27@gmail.com>
2021-06-09 23:41:05 -0700

    Pretty-print embeds when local testing

--- test/context.py
+++ test/context.py
@@ -19,6 +19,27 @@ class Context:
         if content:
             print(f"content={content}")
         if embed:
-            print(f"embed={embed.to_dict()}")
+            print_embed(embed)
         if kwargs and len(kwargs) > 0:
             print(f"kwargs={kwargs}")
+
+
+def print_embed(embed):
+    sep = "\n" + "-" * 40
+
+    embed_dict = embed.to_dict()
+
+    print("Embed")
+    print("\tTitle:", embed_dict['title'])
+    print("\tUrl:", embed_dict['url'])
+    print("\tColor:", embed_dict['color'])
+    print("\tThumbnail Url:", embed_dict['thumbnail']['url'])
+    print("\tDescription:", embed_dict['description'])
+    print("\tFields:")
+    for panel in embed_dict['fields']:
+        print("\t\tInline:", panel['inline'])
+        print("\t\tName:", panel['name'])
+        print("\t\tValue:", sep)
+        print(panel['value'], sep)
+    print("\tDescription:", sep)
+    print(embed_dict['description'], sep)
